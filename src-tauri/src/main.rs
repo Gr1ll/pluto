43085aca8d2455ebe7507ff76c7536372e50db1c
Gr1ll <cyril.kurmann@gmail.com>
2024-06-10 23:30:25 +0200

    started config file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use rustc_serialize::json::Json;
+use std::fs::File;
+use std::io:: {stdout, copy};
+use std::path:: Path;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -8,6 +12,18 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    if Path::new("text.json").exists() {
+        println!("{:?}", "file exists");
+    } else {
+        println!("{:?}", "file does not exist");
+    }
+    let mut file = File::open("text.json").unwrap();
+
+    let mut stdout = stdout();
+    let str = &copy(&mut file, &mut stdout).unwrap().to_string();
+    let _data = Json::from_str(str).unwrap();
+    println!("{:?}", "file");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
