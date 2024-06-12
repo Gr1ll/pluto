5ed89c50e02ba1c9c2388fdc5b9d05a5e23a7acc
Gr1ll <cyril.kurmann@gmail.com>
2024-06-12 23:07:36 +0200

    Application path

--- src-tauri/src/create_document.rs
+++ src-tauri/src/create_document.rs
@@ -1,27 +1,43 @@
 use std::fs::File;
 use std::io::{Read, Write};
-use std::path::Path;
 use rustc_serialize::json::Json;
 use serde_json::{json, Value};
+use std::path::PathBuf;
+use dirs::data_dir;
+
+pub fn get_app_data_dir() -> PathBuf {
+    let mut path = data_dir().expect("Unable to get application data directory");
+    path.push("pluto");
+    path
+}
 
 pub fn check_if_files_exist() {
-    if !Path::new("notes.json").exists() {
-        File::create("notes.json").unwrap();
+    let app_data_dir = get_app_data_dir();
+    print!("{:?}", app_data_dir);
+    let notes_path = app_data_dir.join("notes.json");
+    let config_path = app_data_dir.join("config.json");
+
+    if !notes_path.exists() {
+        let mut file = File::create(&notes_path).unwrap();
         let json = Json::from_str("{\"notes\":[]}").unwrap();
-        let mut file = File::create("notes.json").unwrap();
         file.write_all(json.to_string().as_bytes()).unwrap();
     }
-    if !Path::new("config.json").exists() {
-        File::create("config.json").unwrap();
+
+    if !config_path.exists() {
+        File::create(config_path).unwrap();
     }
 }
 
 pub fn create_note_document(document_name: &str) -> bool {
+    let app_data_dir = get_app_data_dir();
+    let document_path = app_data_dir.join(format!("{}.json", document_name));
+    let notes_path = app_data_dir.join("notes.json");
+
     let json = Json::from_str("{\"name\":\"John Doe\"}").unwrap();
-    let mut file = File::create(document_name + ".json").unwrap();
+    let mut file = File::create(&document_path).unwrap();
     file.write_all(json.to_string().as_bytes()).unwrap();
 
-    let mut notes_file = File::open("notes.json").expect("Unable to open file");
+    let mut notes_file = File::open(&notes_path).expect("Unable to open file");
     let mut notes_data = String::new();
     notes_file.read_to_string(&mut notes_data).expect("Unable to read file");
 
@@ -35,7 +51,7 @@ pub fn create_note_document(document_name: &str) -> bool {
 
     notes_array.push(new_note);
 
-    let mut notes_file = File::create("notes.json").expect("Unable to create file");
+    let mut notes_file = File::create(notes_path).expect("Unable to create file");
     notes_file.write_all(notes_json.to_string().as_bytes()).expect("Unable to write to file");
-    return true;
+    true
 }
