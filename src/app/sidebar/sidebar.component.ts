9d89940380bf50b7b99eab33519e68f8271771b4
Gr1ll <cyril.kurmann@gmail.com>
2024-06-13 11:52:23 +0200

    reversed json list

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -30,5 +30,6 @@ export class SidebarComponent implements OnInit {
     this.Documents = await invoke("get_documents").then((res: string | any) => {
       return JSON.parse(res);
     });
+    this.Documents?.notes.reverse();
   }
 }
