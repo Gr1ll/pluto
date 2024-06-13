1083d6262d7d7d52ae70d1f12ee5cea48f6304f6
Gr1ll <cyril.kurmann@gmail.com>
2024-06-13 21:04:16 +0200

    markdown toggle

--- src/app/note-file/note-file.component.ts
+++ src/app/note-file/note-file.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit, Renderer2 } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Subscription } from "rxjs";
 import { MarkdownPipe } from "../pipe/markdown.pipe";
@@ -14,17 +14,32 @@ import { FormsModule } from "@angular/forms";
 export class NoteFileComponent implements OnInit, OnDestroy {
   private routeSub!: Subscription;
   markdownContent: string = `# Hello, Markdown!`;
+  isEditing: boolean = true;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.routeSub = this.route.params.subscribe((params) => {
-      console.log(params["noteId"]); //log the value of id
+      console.log(params["noteId"]);
     });
   }
 
   updateContent(newContent: string): void {
     this.markdownContent = newContent;
+  }
+
+  toggleEdit(): void {
+    this.isEditing = !this.isEditing;
+    if (this.isEditing) {
+      setTimeout(() => {
+        const textArea = document.getElementById("markdown-editor");
+        textArea?.focus();
+      });
+    }
+  }
+
+  toggleView(): void {
+    this.isEditing = false;
   }
 
   ngOnDestroy() {
