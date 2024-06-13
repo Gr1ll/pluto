6dcc084b1c3a77079188cc2f3c2f3f5da991204b
Gr1ll <cyril.kurmann@gmail.com>
2024-06-13 08:16:27 +0200

    improoved folder structure

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-mod create_document;
-mod write_document;
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Me!", name)
-}
-
-#[tauri::command]
-fn get_document_creation(document_name: &str) -> String {
-    create_document::create_note_document(document_name);
-    format!("Hello, {}! You've been greeted from Me!", document_name)
-}
+mod doc_manager;
+mod cmd_handler;
 
 fn main() {
-    create_document::check_if_files_exist();
+    doc_manager::create_document::check_if_files_exist();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, get_document_creation])
+        .invoke_handler(tauri::generate_handler![cmd_handler::ts_cmds::get_document_creation])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
