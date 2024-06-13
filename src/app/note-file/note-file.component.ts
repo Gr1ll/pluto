51234f4559d29f140d9ac17a8bb98a36a1b3804d
Gr1ll <cyril.kurmann@gmail.com>
2024-06-13 20:51:53 +0200

    added markdown

--- src/app/note-file/note-file.component.ts
+++ src/app/note-file/note-file.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Subscription } from "rxjs";
+import { MarkdownPipe } from "../pipe/markdown.pipe";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-note-file",
   standalone: true,
-  imports: [],
+  imports: [MarkdownPipe, FormsModule],
   templateUrl: "./note-file.component.html",
   styleUrl: "./note-file.component.css",
 })
 export class NoteFileComponent implements OnInit, OnDestroy {
   private routeSub!: Subscription;
+  markdownContent: string = `# Hello, Markdown!`;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -18,6 +21,10 @@ export class NoteFileComponent implements OnInit, OnDestroy {
     this.routeSub = this.route.params.subscribe((params) => {
       console.log(params["noteId"]); //log the value of id
     });
+  }
+
+  updateContent(newContent: string): void {
+    this.markdownContent = newContent;
   }
 
   ngOnDestroy() {
